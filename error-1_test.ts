7e18082a6a0f629b13d79203b5b8e9396d829f30
Cyril Hue <cy.hue80@gmail.com>
2024-05-23 23:03:03 +0200

    first commit

---
+++ error-1_test.ts
@@ -0,0 +1,73 @@
+Feature("homepage").tag("@error1");
+
+Before(async ({ I }) => {
+  I.amOnPage("https://www.google.com");
+
+  setTimeout(() => {
+    throw new Error("Async error");
+  }, 1000);
+});
+
+After(async ({ I }) => {
+  console.log("After hook");
+
+  setTimeout(() => {
+    throw new Error("Async error");
+  }, 1000);
+});
+
+Scenario("test something 1", ({ I }) => {
+  const buttonRefuseModal = locate("button#W0wltc");
+  I.seeElement(buttonRefuseModal);
+  I.click(buttonRefuseModal);
+  I.dontSeeElement(buttonRefuseModal);
+});
+Scenario("test something 2", ({ I }) => {
+  const buttonRefuseModal = locate("button#W0wltc");
+  I.seeElement(buttonRefuseModal);
+  I.click(buttonRefuseModal);
+  I.dontSeeElement(buttonRefuseModal);
+});
+Scenario("test something 3", ({ I }) => {
+  const buttonRefuseModal = locate("button#W0wltc");
+  I.seeElement(buttonRefuseModal);
+  I.click(buttonRefuseModal);
+  I.dontSeeElement(buttonRefuseModal);
+});
+Scenario("test something 4", ({ I }) => {
+  const buttonRefuseModal = locate("button#W0wltc");
+  I.seeElement(buttonRefuseModal);
+  I.click(buttonRefuseModal);
+  I.dontSeeElement(buttonRefuseModal);
+});
+Scenario("test something 5", ({ I }) => {
+  const buttonRefuseModal = locate("button#W0wltc");
+  I.seeElement(buttonRefuseModal);
+  I.click(buttonRefuseModal);
+  I.dontSeeElement(buttonRefuseModal);
+});
+Scenario("test something 6", ({ I }) => {
+  const buttonRefuseModal = locate("button#W0wltc");
+  I.seeElement(buttonRefuseModal);
+  I.click(buttonRefuseModal);
+  I.dontSeeElement(buttonRefuseModal);
+});
+Scenario("test something 7", ({ I }) => {
+  const buttonRefuseModal = locate("button#W0wltc");
+  I.seeElement(buttonRefuseModal);
+  I.click(buttonRefuseModal);
+  I.dontSeeElement(buttonRefuseModal);
+  I.seeElement(buttonRefuseModal);
+});
+Scenario("test something 8", ({ I }) => {
+  const buttonRefuseModal = locate("button#W0wltc");
+  I.seeElement(buttonRefuseModal);
+  I.click(buttonRefuseModal);
+  I.dontSeeElement(buttonRefuseModal);
+});
+Scenario("test something 9", ({ I }) => {
+  const buttonRefuseModal = locate("button#W0wltc");
+  I.seeElement(buttonRefuseModal);
+  I.click(buttonRefuseModal);
+  I.dontSeeElement(buttonRefuseModal);
+});
---
+++ error-3_test.ts
@@ -0,0 +1,76 @@
+Feature("homepage").tag("@error3");
+
+Before(async ({ I }) => {
+  I.amOnPage("https://www.google.com");
+});
+
+Scenario("test something 1", ({ I }) => {
+  const buttonRefuseModal = locate("button#W0wltc");
+  I.seeElement(buttonRefuseModal);
+  I.click(buttonRefuseModal);
+  I.dontSeeElement(buttonRefuseModal);
+});
+Scenario("test something 2", ({ I }) => {
+  const buttonRefuseModal = locate("button#W0wltc");
+  try {
+    setTimeout(() => {
+      throw new Error("Async error");
+    }, 1000);
+  } catch (e) {
+    console.log(e);
+  }
+
+  I.seeElement(buttonRefuseModal);
+  I.click(buttonRefuseModal);
+  I.dontSeeElement(buttonRefuseModal);
+});
+Scenario("test something 3", ({ I }) => {
+  const buttonRefuseModal = locate("button#W0wltc");
+  try {
+    setTimeout(() => {
+      throw new Error("Async error");
+    }, 1000);
+  } catch (e) {
+    console.log(e);
+  }
+  I.seeElement(buttonRefuseModal);
+  I.click(buttonRefuseModal);
+  I.dontSeeElement(buttonRefuseModal);
+});
+Scenario("test something 4", ({ I }) => {
+  const buttonRefuseModal = locate("button#W0wltc");
+  I.seeElement(buttonRefuseModal);
+  I.click(buttonRefuseModal);
+  I.dontSeeElement(buttonRefuseModal);
+});
+Scenario("test something 5", ({ I }) => {
+  const buttonRefuseModal = locate("button#W0wltc");
+  I.seeElement(buttonRefuseModal);
+  I.click(buttonRefuseModal);
+  I.dontSeeElement(buttonRefuseModal);
+});
+Scenario("test something 6", ({ I }) => {
+  const buttonRefuseModal = locate("button#W0wltc");
+  I.seeElement(buttonRefuseModal);
+  I.click(buttonRefuseModal);
+  I.dontSeeElement(buttonRefuseModal);
+});
+Scenario("test something 7", ({ I }) => {
+  const buttonRefuseModal = locate("button#W0wltc");
+  I.seeElement(buttonRefuseModal);
+  I.click(buttonRefuseModal);
+  I.dontSeeElement(buttonRefuseModal);
+  I.seeElement(buttonRefuseModal);
+});
+Scenario("test something 8", ({ I }) => {
+  const buttonRefuseModal = locate("button#W0wltc");
+  I.seeElement(buttonRefuseModal);
+  I.click(buttonRefuseModal);
+  I.dontSeeElement(buttonRefuseModal);
+});
+Scenario("test something 9", ({ I }) => {
+  const buttonRefuseModal = locate("button#W0wltc");
+  I.seeElement(buttonRefuseModal);
+  I.click(buttonRefuseModal);
+  I.dontSeeElement(buttonRefuseModal);
+});
